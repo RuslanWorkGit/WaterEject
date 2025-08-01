769b9da9b3004b5754745fa45854241a42e2f7d1
RuslanWorkGit <lulka758@gmail.com>
2025-08-01 17:40:41 +0300

    add screens View

---
+++ WaterEject/WaterEject/View/Modes/ModesView.swift
@@ -0,0 +1,18 @@
+//
+//  ModesView.swift
+//  WaterEject
+//
+//  Created by Ruslan Liulka on 01.08.2025.
+//
+
+import SwiftUI
+
+struct ModesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ModesView()
+}
---
+++ WaterEject/WaterEject/View/Start/StartView.swift
@@ -0,0 +1,18 @@
+//
+//  StartView.swift
+//  WaterEject
+//
+//  Created by Ruslan Liulka on 01.08.2025.
+//
+
+import SwiftUI
+
+struct StartView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StartView()
+}
