e64dd41a9604b37071ba6c2bc5e15fa21e1d21c2
RuslanWorkGit <lulka758@gmail.com>
2025-08-06 22:09:40 +0300

    update solutionView

--- WaterEject/WaterEject/OnBoarding/SolutionView.swift
+++ WaterEject/WaterEject/OnBoarding/SolutionView.swift
@@ -9,7 +9,38 @@ import SwiftUI
 
 struct SolutionView: View {
     var body: some View {
-        Text("SolutionView")
+        ZStack {
+            Background(startCleaning: true)
+            
+            VStack(alignment: .center) {
+                (
+                    Text("Use Safe Sound Frequencies to")
+                        .foregroundStyle(Color(red: 238 / 255, green: 255 / 255, blue: 236 / 255)) +
+                    Text(" Push Water Out")
+                        .foregroundStyle(Color(red: 43 / 255, green: 217 / 255, blue: 156 / 255))
+      
+                )
+                .font(.system(size: 32, weight: .bold))
+                .multilineTextAlignment(.center)
+                .padding(.horizontal, 24)
+                .padding(.bottom, 54)
+                
+
+                ZStack(alignment: .trailing) {
+                    Image("devicesBig")
+                    Image("Aprove")
+                        .offset(x: 52)
+                        
+                }
+                
+                
+            }
+            .frame(maxHeight: .infinity, alignment: .top)
+            .padding(.horizontal, 12)
+            .padding(.top, 50)
+            
+        }
+
     }
 }
 
