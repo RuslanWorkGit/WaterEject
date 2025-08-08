5e4e8997aa20b63f0098e09ee806d2f6b4f048bc
RuslanWorkGit <lulka758@gmail.com>
2025-08-08 22:25:18 +0300

    Update BassView.swift

--- WaterEject/WaterEject/View/TestView/Bass/BassView.swift
+++ WaterEject/WaterEject/View/TestView/Bass/BassView.swift
@@ -9,7 +9,20 @@ import SwiftUI
 
 struct BassView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Image("Lines")
+            
+            VStack(spacing: 15) {
+                Text("32dB")
+                    .font(.system(size: 80, weight: .bold))
+                    .foregroundStyle(Color(red: 238 / 255, green: 255 / 255, blue: 236 / 255))
+                ZStack {
+                    Image("BlueWave")
+                    Image("BlackWave")
+                }
+            }
+            .offset(y: -20)
+        }
     }
 }
 
