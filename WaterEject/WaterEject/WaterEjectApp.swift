2aeb93415d93c03c1403128fce5f0b6d2d13a7a1
RuslanWorkGit <lulka758@gmail.com>
2025-10-31 00:16:10 +0200

    update analitics

--- WaterEject/WaterEject/WaterEjectApp.swift
+++ WaterEject/WaterEject/WaterEjectApp.swift
@@ -9,9 +9,66 @@ import SwiftUI
 import RevenueCat
 import Firebase
 import FirebaseFirestore
+import AppsFlyerLib
+
+// MARK: - AppDelegate для AppsFlyer (і, за потреби, диплінків)
+final class AppDelegate: NSObject, UIApplicationDelegate {
+
+    // ВСТАВ свої значення
+    private let appsFlyerDevKey = "mxUTQbads3dmAtKCADioKm"
+    private let appleAppID      = "6749094272" // напр. "1234567890"
+
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil
+    ) -> Bool {
+
+        // 1) Базова конфігурація AF
+        let af = AppsFlyerLib.shared()
+        af.appsFlyerDevKey = appsFlyerDevKey
+        af.appleAppID = appleAppID
+
+        // 2) Зв'язуємо юзерID з RevenueCat (той самий ID у всіх системах)
+        af.customerUserID = Purchases.shared.appUserID
+        
+        print("AppsFlyer UID:", AppsFlyerLib.shared().getAppsFlyerUID())
+
+        printIDFV()
+        
+        #if DEBUG
+        af.isDebug = true
+        #endif
+        return true
+    }
+
+    // 3) Старт сесії якомога раніше (коли апка активна)
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        AppsFlyerLib.shared().start()
+    }
+
+    // 4) (Необов’язково) якщо будуть OneLink/Universal Links
+    func application(_ app: UIApplication,
+                     open url: URL,
+                     options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        AppsFlyerLib.shared().handleOpen(url, options: options)
+        return false
+    }
+    
+    func printIDFV() {
+        if let idfv = UIDevice.current.identifierForVendor?.uuidString {
+            print("IDFV:", idfv)
+        } else {
+            print("IDFV is nil")
+        }
+    }
+}
+
 
 @main
 struct WaterEjectApp: App {
+    
+    // Підключаємо делегат до SwiftUI-життєвого циклу
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     
     init() {
         OneTimeDefaultsReset.run(full: true)
